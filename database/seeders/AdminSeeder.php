00360169bfa3783ee86a951c909b0e06b6dce928
João Pedro <joaopedromeiramessias@gmail.com>
2023-06-22 21:57:56 -0300

    aLTERAÇÕES

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+    }
+}
